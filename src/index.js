c32406e3bf482f1f5450b30c90244d01c9aaa8c1
pallavi15 <pallavigaharwar15@gmail.com>
2025-03-04 12:32:39 +0530

    [Pallavi] Add . Added validation for contact details

--- src/index.js
+++ src/index.js
@@ -5,17 +5,21 @@ const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
-        "Pallavi", "Singh", "Bhopal", "India", "In", "100001", "1345678654", "pallavigaharwar15@gmail.com"
+        "Pallavi", "Singh", "Bhopal", "India", "In", "100001", "1345678654", "pallavigahrwar15@gmail.com"
     );
-    console.log(addressBook.addContact(contact1));
-    console.log(addressBook.getAllContacts());
+    console.log(addressBook.addContact(contact1)); 
+
+    const contact2 = new Contact(
+        "Sujal","Singh","Bhopal","India", "In", "100001", "123456780","sujalsingh@gmail.com");
+    console.log(addressBook.addContact(contact2)); 
+    console.log("All Contacts", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
 
 try {
     const invalidContact = new Contact(
-        "pa", "singh", "abc", "In", "I", "100A01", "12345", "invalid-email"
+        "pal","sin","In", "I", "100A01", "12345", "invalid-email"
     );
     console.log(addressBook.addContact(invalidContact));
 } catch (error) {
