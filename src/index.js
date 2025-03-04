9d5d108b7ca1eb5b85945b5d07b5236dd82ca175
pallavi15 <pallavigaharwar15@gmail.com>
2025-03-04 12:09:57 +0530

    [Pallavi] Add . Added validation for contact details using Exceptions

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ const Contact = require('./models/Contacts');
 
 const addressBook = new AddressBook();
 
-const contact1 = new Contact(
-    "Pallavi", "Singh", "Bhopal", "India", "In", "10001", "1345678654", "pallavigaharwar15@gmail.com"
-);
-console.log(addressBook.addContact(contact1));
-console.log(addressBook.getAllContacts());
+try {
+    const contact1 = new Contact(
+        "Pallavi", "Singh", "Bhopal", "India", "In", "100001", "1345678654", "pallavigaharwar15@gmail.com"
+    );
+    console.log(addressBook.addContact(contact1));
+    console.log(addressBook.getAllContacts());
+} catch (error) {
+    console.error("Error:", error.message);
+}
+
+try {
+    const invalidContact = new Contact(
+        "pa", "singh", "abc", "In", "I", "100A01", "12345", "invalid-email"
+    );
+    console.log(addressBook.addContact(invalidContact));
+} catch (error) {
+    console.error("Error:", error.message);
+}
